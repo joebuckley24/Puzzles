c3ac5bdfd7b770ea18c172344b282c14387b6b9a
Joe Buckley <joebuckley24@gmail.com>
2024-06-11 17:50:54 -0400

    found acceptable solution to original puzzle

--- puzzle.py
+++ puzzle.py
@@ -2,28 +2,33 @@ import numpy as np
 import pandas as pd
 import random
 import copy
+import pickle
 STATES = pd.read_csv("states_pops.csv")
 SOLUTION = ""
 
 def score(answer):
-	poss_states = find_states(answer)
+	poss_states, sqs_used = find_states(answer)
 	score = {}
 	for row in poss_states:
 		for li in row:
 			for state in li:
 				if state not in score:
 					score[state] = STATES.loc[STATES.state==state, "pop"].values[0]
-	return score
+	return score, sqs_used
+
+def ansr2brd(ansr):
+	return [ansr[5*n:5*(n+1)] for n in range(5)]
 
 def find_states(answer):
-	board = [answer[5*n:5*(n+1)] for n in range(5)]
+	board = ansr2brd(answer)
 	poss_states_bysq = [[list() for _ in range(5)] for _ in range(5)]
+	sqs_used = {}
 	for i in range(5):
 		for j in range(5):
-			find_states_bysq(board, i, j, "", [True]*50, [True]*50, poss_states_bysq[i][j], str(i)+str(j)+" ")
-	return poss_states_bysq
+			find_states_bysq(board, i, j, "", [True]*50, [True]*50, poss_states_bysq[i][j], str(i)+str(j)+" ", sqs_used)
+	return poss_states_bysq, sqs_used
 
-def find_states_bysq(board, i, j, s, poss, perf, ansr_ij, ij_path):
+def find_states_bysq(board, i, j, s, poss, perf, ansr_ij, ij_path, sqs_used):
 	poss_ = poss[:]
 	perf_ = perf[:]
 	# print(f"ansr_ij={ansr_ij}")
@@ -49,12 +54,17 @@ def find_states_bysq(board, i, j, s, poss, perf, ansr_ij, ij_path):
 				if len(s) + 1 == len(stt):
 					ansr_ij.append(stt)
 					poss_[k] = False
+					for ij in ij_path.split():
+						if ij in sqs_used:
+							sqs_used[ij].add(stt)
+						else:
+							sqs_used[ij] = {stt}
 	if any(poss_):
 		for di, dj in [(-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0)]:
 			i_ = i + di
 			j_ = j + dj
 			if i_ >= 0 and i_ < 5 and j_ >= 0 and j_ < 5:
-				find_states_bysq(board, i_, j_, s+board[i][j], poss_, perf_, ansr_ij, ij_path+str(i_)+str(j_)+" ")
+				find_states_bysq(board, i_, j_, s+board[i][j], poss_, perf_, ansr_ij, ij_path+str(i_)+str(j_)+" ", sqs_used)
 
 def def_distr():
 	counts = {}
@@ -99,31 +109,32 @@ def gen_cand(letters, probs):
 def gen_muttn(letters, probs):
 	return "".join(np.random.choice(letters, 1, p=probs))
 
-def mutate(cand, cond_distr):
-	i, j = divmod(random.choice(range(25)))
-	counts = {}
+def mutate(cand, cond_cnts):
+	idx = random.choice(range(25))
+	i, j = divmod(idx, 5)
+	cnts = {}
 	for di, dj in [(-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0)]:
 		i_ = i + di
 		j_ = j + dj
 		if i_ >= 0 and i_ < 5 and j_ >= 0 and j_ < 5:
-			neighb = cand[i][j]
-			for el, count in cond_distr[neighb]:
-				if el not in counts:
-					counts[el] = count
+			idx_ = 5*i_ + j_
+			neighb = cand[idx_]
+			# print(cond_cnts)
+			# print(cond_cnts[neighb])
+			for ltr, cnt in cond_cnts[neighb].items():
+				if ltr not in cnts:
+					cnts[ltr] = cnt
 				else:
-					counts[el] += count
-	letters = np.fromiter(counts.keys(), dtype=object)
-	probs = np.fromiter(counts.values(), dtype=float) / sum(counts.values())
-	cand[i][j] = "".join(np.random.choice(letters, 1, p=probs))
+					cnts[ltr] += cnt
+	letters = np.fromiter(cnts.keys(), dtype=object)
+	probs = np.fromiter(cnts.values(), dtype=float) / sum(cnts.values())
+	mut = "".join(np.random.choice(letters, 1, p=probs))
+	return cand[:idx] + mut + cand[idx+1:]
 
 def crossover(cand1, cand2):
-	i, j = divmod(random.randrange(23), 5)
-	cand1_ = [cand1[row][:] if row < i else 
-			  cand1[row][:j+1] + cand2[row][j+1:] if row == i else 
-			  cand2[row][:] for row in range(5)]
-	cand2_ = [cand2[row][:] if row < i else 
-			  cand2[row][:j+1] + cand1[row][j+1:] if row == i else 
-			  cand1[row][:] for row in range(5)]
+	split = random.randrange(23)
+	cand1_ = cand1[:split+1] + cand2[split+1:]
+	cand2_ = cand2[:split+1] + cand1[split+1:]
 	return cand1_, cand2_
 
 def print_board(s25):
@@ -135,7 +146,13 @@ def print_scores(dic):
 	for k,v in dic.items():
 		print(f"{k}: {v}")
 
-if __name__ == "__main__":
+def print_output(tup):
+	print_board(tup[0])
+	print_scores(tup[1])
+	print(f"total={tup[2]}")
+
+# if __name__ == "__main__":
+if False:
 	# answer = \
 	# "tho__" + \
 	# "ain__" + \
@@ -147,17 +164,99 @@ if __name__ == "__main__":
 	# print(sum(scr.values()))
 
 	letters, probs = def_distr()
-	pos_scores = []
-	for i in range(100_000):
-		cand = gen_cand(letters, probs)
-		scrs = score(cand)
-		scr = sum(scrs.values())
-		if scr > 0:
-			pos_scores.append((cand,scrs,scr))
+	cond_counts = def_cond_distr()
+
+	try:
+		with(open("pos_scores.pickle","rb")) as f:
+			pos_scores = pickle.load(f)
+	except FileNotFoundError:
+		pos_scores = []
+		for i in range(10_000): #originally 100k
+			cand = gen_cand(letters, probs)
+			scrs, _ = score(cand)
+			scr = sum(scrs.values())
+			if scr > 0:
+				pos_scores.append((cand,scrs,scr))
 
 	top5 = sorted(pos_scores, key=lambda t: t[2], reverse=True)[:5]
+	# for i in range(5):
+	# 	print_board(top5[i][0])
+	# 	print_scores(top5[i][1])
+	# 	print(top5[i][2])
+	# 	print("")
+
+	# print_board(mutate(top5[-1][0], cond_counts))
+	# print("")
+	# c1, c2 = crossover(top5[-2][0], top5[-1][0])
+	# print_board(c1)
+	# print_board("")
+	# print_board(c2)
+
+	rounds = 8
+	top_n = 5000
+	top = sorted(pos_scores, key=lambda t: t[2], reverse=True)[:top_n]
+	for i in range(rounds):
+		idxs = list(range(top_n))
+		random.shuffle(idxs)
+		for i, j in zip(idxs[:top_n//2], idxs[top_n//2:]):
+			c1, c2 = crossover(top[i][0], top[j][0])
+			c1 = mutate(c1, cond_counts)
+			c2 = mutate(c2, cond_counts)
+			c1_scores, c1_sqs = score(c1)
+			c1_score = sum(c1_scores.values())
+			c2_scores, c2_sqs = score(c2)
+			c2_score = sum(c2_scores.values())
+			top += [(c1,c1_scores,c1_score), (c2,c2_scores,c2_score)]
+		top = sorted(top, key=lambda t: t[2], reverse=True)[:top_n]
+
+	top5 = sorted(top, key=lambda t: t[2], reverse=True)[:5]
 	for i in range(5):
 		print_board(top5[i][0])
 		print_scores(top5[i][1])
 		print(top5[i][2])
 		print("")
+
+if __name__ == "__main__":
+
+	letters, probs = def_distr()
+	cond_counts = def_cond_distr()
+
+	with(open("top1000.pickle","rb")) as f:
+		top1000 = pickle.load(f)
+	# c1, c2 = crossover(top1000[0][0], top1000[1][0])
+	# c1_scores, c1_sqs = score(top1000[0][0])
+	# c1_score = sum(c1_scores.values())
+	# c2_scores, c2_sqs = score(top1000[1][0])
+	# c2_score = sum(c2_scores.values())
+
+	for i in range(5):
+		c1_scores, c1_sqs = score(top1000[i][0])
+		for k, v in sorted(c1_sqs.items()):
+			print(k + ": " + str(len(v)))
+		print("")
+
+	for i in range(5):
+		c1_scores, c1_sqs = score(top1000[i][0])
+		print(len(c1_sqs))
+
+	print_output(top1000[4])
+
+	bstr = "wtimkensvfkoalircsrodnain"
+	best = sum(score(bstr)[0].values())
+	for i in range(25):
+		instr = "".join([bstr[:i] + "_" + bstr[i+1:]])
+		print_board(instr)
+		best - sum(score(instr)[0].values())
+
+	for i in [4, 20]:
+		print("")
+		print(i)
+		for num in range(ord('a'),ord('z')+1):
+			ltr = chr(num)
+			instr = bstr[:i] + ltr + bstr[i+1:]
+			new_scr = sum(score(instr)[0].values())
+			print(ltr + ": " + str(new_scr))
+
+	new_bstr = "wtimaensvfkoalircsroenain"
+	new_best = sum(score(new_bstr)[0].values())
+
