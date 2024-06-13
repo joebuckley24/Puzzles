3241284277b7ef081cd508fc8390f8391dd647c1
Joseph Buckley <Joseph Buckley@bluefintrading.com>
2024-06-13 19:08:05 -0400

    clean up main section and finish writeup

--- puzzle.py
+++ puzzle.py
@@ -151,112 +151,75 @@ def print_output(tup):
 	print_scores(tup[1])
 	print(f"total={tup[2]}")
 
-# if __name__ == "__main__":
-if False:
-	# answer = \
-	# "tho__" + \
-	# "ain__" + \
-	# "esl__" + \
-	# "_____" + \
-	# "_____"
-	# scr = score(answer)
-	# print(scr)
-	# print(sum(scr.values()))
 
-	letters, probs = def_distr()
-	cond_counts = def_cond_distr()
 
-	try:
-		with(open("pos_scores.pickle","rb")) as f:
-			pos_scores = pickle.load(f)
-	except FileNotFoundError:
-		pos_scores = []
-		for i in range(10_000): #originally 100k
-			cand = gen_cand(letters, probs)
-			scrs, _ = score(cand)
-			scr = sum(scrs.values())
-			if scr > 0:
-				pos_scores.append((cand,scrs,scr))
-
-	top5 = sorted(pos_scores, key=lambda t: t[2], reverse=True)[:5]
-	# for i in range(5):
-	# 	print_board(top5[i][0])
-	# 	print_scores(top5[i][1])
-	# 	print(top5[i][2])
-	# 	print("")
-
-	# print_board(mutate(top5[-1][0], cond_counts))
-	# print("")
-	# c1, c2 = crossover(top5[-2][0], top5[-1][0])
-	# print_board(c1)
-	# print_board("")
-	# print_board(c2)
-
-	rounds = 8
-	top_n = 5000
-	top = sorted(pos_scores, key=lambda t: t[2], reverse=True)[:top_n]
-	for i in range(rounds):
-		idxs = list(range(top_n))
-		random.shuffle(idxs)
-		for i, j in zip(idxs[:top_n//2], idxs[top_n//2:]):
-			c1, c2 = crossover(top[i][0], top[j][0])
-			c1 = mutate(c1, cond_counts)
-			c2 = mutate(c2, cond_counts)
-			c1_scores, c1_sqs = score(c1)
-			c1_score = sum(c1_scores.values())
-			c2_scores, c2_sqs = score(c2)
-			c2_score = sum(c2_scores.values())
-			top += [(c1,c1_scores,c1_score), (c2,c2_scores,c2_score)]
-		top = sorted(top, key=lambda t: t[2], reverse=True)[:top_n]
-
-	top5 = sorted(top, key=lambda t: t[2], reverse=True)[:5]
-	for i in range(5):
-		print_board(top5[i][0])
-		print_scores(top5[i][1])
-		print(top5[i][2])
-		print("")
 
 if __name__ == "__main__":
 
 	letters, probs = def_distr()
 	cond_counts = def_cond_distr()
 
-	with(open("top1000.pickle","rb")) as f:
-		top1000 = pickle.load(f)
-	# c1, c2 = crossover(top1000[0][0], top1000[1][0])
-	# c1_scores, c1_sqs = score(top1000[0][0])
-	# c1_score = sum(c1_scores.values())
-	# c2_scores, c2_sqs = score(top1000[1][0])
-	# c2_score = sum(c2_scores.values())
+	## load cached list of candidates
+	try:
+		with(open("top1000.pickle","rb")) as f:
+			top1000 = pickle.load(f)
+	except FileNotFoundError:
+		try:
+			with(open("pos_scores.pickle","rb")) as f:
+				pos_scores = pickle.load(f)
+		except FileNotFoundError:
+			pos_scores = []
+			for i in range(10_000): #originally 100k
+				cand = gen_cand(letters, probs)
+				scrs, _ = score(cand)
+				scr = sum(scrs.values())
+				if scr > 0:
+					pos_scores.append((cand,scrs,scr))
+		rounds = 8
+		top_n = 5000
+		top = sorted(pos_scores, key=lambda t: t[2], reverse=True)[:top_n]
+		for i in range(rounds):
+			idxs = list(range(top_n))
+			random.shuffle(idxs)
+			for i, j in zip(idxs[:top_n//2], idxs[top_n//2:]):
+				c1, c2 = crossover(top[i][0], top[j][0])
+				c1 = mutate(c1, cond_counts)
+				c2 = mutate(c2, cond_counts)
+				c1_scores, c1_sqs = score(c1)
+				c1_score = sum(c1_scores.values())
+				c2_scores, c2_sqs = score(c2)
+				c2_score = sum(c2_scores.values())
+				top += [(c1,c1_scores,c1_score), (c2,c2_scores,c2_score)]
+			top = sorted(top, key=lambda t: t[2], reverse=True)[:top_n]
+		top1000 = top[:1000]
 
-	for i in range(5):
-		c1_scores, c1_sqs = score(top1000[i][0])
-		for k, v in sorted(c1_sqs.items()):
-			print(k + ": " + str(len(v)))
-		print("")
+	# for i in range(5):
+	# 	c1_scores, c1_sqs = score(top1000[i][0])
+	# 	for k, v in sorted(c1_sqs.items()):
+	# 		print(k + ": " + str(len(v)))
+	# 	print("")
 
-	for i in range(5):
-		c1_scores, c1_sqs = score(top1000[i][0])
-		print(len(c1_sqs))
+	# for i in range(5):
+	# 	c1_scores, c1_sqs = score(top1000[i][0])
+	# 	print(len(c1_sqs))
 
-	print_output(top1000[4])
+	# print_output(top1000[4])
 
-	bstr = "wtimkensvfkoalircsrodnain"
-	best = sum(score(bstr)[0].values())
-	for i in range(25):
-		instr = "".join([bstr[:i] + "_" + bstr[i+1:]])
-		print_board(instr)
-		best - sum(score(instr)[0].values())
+	# bstr = "wtimkensvfkoalircsrodnain"
+	# best = sum(score(bstr)[0].values())
+	# for i in range(25):
+	# 	instr = "".join([bstr[:i] + "_" + bstr[i+1:]])
+	# 	print_board(instr)
+	# 	best - sum(score(instr)[0].values())
 
-	for i in [4, 20]:
-		print("")
-		print(i)
-		for num in range(ord('a'),ord('z')+1):
-			ltr = chr(num)
-			instr = bstr[:i] + ltr + bstr[i+1:]
-			new_scr = sum(score(instr)[0].values())
-			print(ltr + ": " + str(new_scr))
+	# for i in [4, 20]:
+	# 	print("")
+	# 	print(i)
+	# 	for num in range(ord('a'),ord('z')+1):
+	# 		ltr = chr(num)
+	# 		instr = bstr[:i] + ltr + bstr[i+1:]
+	# 		new_scr = sum(score(instr)[0].values())
+	# 		print(ltr + ": " + str(new_scr))
 
 	new_bstr = "wtimaensvfkoalircsroenain"
 	new_best = sum(score(new_bstr)[0].values())
-
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
-## Altered States 2 ##
+# Altered States 2 #
 
-### Jane Street Puzzle June 2024, Solved by Joe Buckley ###
+## Jane Street June 2024 Puzzle, solved by Joe Buckley ##
+
+Please go to the June 2024 entry in Jane Street's [Puzzle Archive](https://www.janestreet.com/puzzles/archive/ "archive") for the official prompt and other months' puzzles.
+
+### Solved by Joe Buckley ###
 
 ![5x5 blank grid and 3x3 example grid](june-2024.png "Example photo")
 
@@ -18,3 +22,19 @@ Now we’re at it again! Once more, your goal is to score **as many points as po
 The 3-by-3 example above scores **32,913,047 points**, for Illinois (Inlinois), Ohio, Utah (Atah), Iowa (Ioha), and Idaho (Ieaho).
 
 To send in your entry, please render your grid as **one unbroken 25-digit string** by concatenating the rows. (The 3-by-3 grid from the example would be “thoainesl”.)
+
+To qualify for the leaderboard, you entry must score **at least half** of the available points. (So: at least 165,379,868.)
+
+## Solution ##
+
+We need to find an optimal 5x5 arrangement of letters to maximize our score. Since we don't have any training data and can't easily generate any, we'll have to be smarter than just throwing data at a neural net until it spits out the right answer. Instead, we'll have to use an iterative optimization technique to generate, test, and improve our arrangement of letters. This sounds like the perfect job for a **genetic algorithm**. 
+
+The genetic algorithm is inspired by natural selection in the biological world, in which each generation of solutions is iteratively improved via recombination and mutation until an acceptable solution is found. In other words, first we randomly generate solutions. Then, we take pairs of those solutions, split each one in half, and then recombine each of the different halves so we have two new child candidates. Next, in the mutation step we inject noise into the process by regenerating one or more of the letters in a candidate solution so as to broaden the set of paths that we search. Now it's time to score each new solution using the evaluation function that we want to maximaize. Finally, using the parent and child population of solutions, we select the top 50% of candidates, thus keeping the same number of potential solutions. Each iteration we will have a top-scoring candidate, and once we breach our threshold of 165 million, we're done!
+
+After a few hours of running the code, our best string was 
+> WTIMA
+> ENSVF
+> KOALI
+> RCSRO
+> ENAIN
+giving us a score of 188,652,076.
